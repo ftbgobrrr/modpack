3eb7a18bade5b5b8e31deeefa6f8699e988a6fb3
Uriel <imurx@proton.me>
2024-05-03 21:30:31 -0300

    fix ignore and add manual update script

---
+++ build/run.bat
@@ -0,0 +1,1 @@
+java -jar packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/ftbgobrrr/modpack/main/pack.toml
---
+++ build/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/ftbgobrrr/modpack/main/pack.toml
